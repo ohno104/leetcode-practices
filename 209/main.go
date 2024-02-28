737190cb13a59b606baf23ef2d105ffc0fccf736
ohno104 <fly0300582307@gmail.com>
2024-02-29 00:19:04 +0800

    update 209

---
+++ 209/main.go
@@ -0,0 +1,28 @@
+package main
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+func minSubArrayLen(target int, nums []int) int {
+	sum := 0
+	start := 0
+	res := len(nums) + 1 // +1用來判斷有沒有符合條件
+
+	for end := 0; end < len(nums); end++ {
+		sum += nums[end]
+		for sum >= target {
+			subLength := end - start + 1
+			if subLength < res {
+				res = subLength
+			}
+			sum -= nums[start] // 先減才能移動
+			start++
+		}
+	}
+
+	if res > len(nums) {
+		return 0
+	}
+
+	return res
+}
